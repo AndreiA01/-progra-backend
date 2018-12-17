6d26bebf86b9ba4c65534850d4967c0bd1907aa4
AndreitaCanizares <andreita.canizares01@gmail.com>
2018-12-17 09:59:44 -0500

    practica12

---
+++ Practica 12/Datos/Querys.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aceso;
+namespace Datos
+{
+    public class Querys
+    {
+        basePractica12 contexto;
+
+        public Querys()
+        {
+            contexto = new basePractica12();
+            contexto.Configuration.ProxyCreationEnabled = false;
+        }
+
+        public List<personas> SelecAll()
+        {
+            return contexto.personas.ToList();
+        }
+
+        public personas Insertar(personas nueva)
+        {
+            contexto.personas.Add(nueva);
+            contexto.SaveChanges();
+            return nueva;
+        }
+
+        public bool Actualizar(personas datos)
+        {
+            personas aux = contexto.personas.Where(t => t.id_pe == datos.id_pe).SingleOrDefault();
+            if (aux != null)
+            {
+                aux.bio_pe = datos.bio_pe;
+                aux.dni_pe = datos.dni_pe;
+                aux.email_pe = datos.email_pe;
+                aux.fecha_pe = datos.fecha_pe;
+                aux.foto_pe = datos.foto_pe;
+                aux.nombres_pe = datos.nombres_pe;
+                aux.web_pe = datos.web_pe;
+                contexto.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Eliminar(personas datos)
+        {
+            personas aux = contexto.personas.Where(t => t.id_pe == datos.id_pe).SingleOrDefault();
+            if (aux != null)
+            {
+                contexto.personas.Remove(aux);
+                contexto.SaveChanges();
+                return true;
+            }
+            else
+                return false;
+        }
+
+    }
+}
---
+++ Practica 12/Logica/Serializador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aceso;
+using Datos;
+
+namespace Logica
+{
+    public class Serializador
+    {
+        Querys datos = new Querys();
+
+        public List<personas> SeleccionarTodas() => datos.SelecAll();
+        public bool ActualizarPersona(personas persona) => datos.Actualizar(persona);
+        public bool EliminarPersona(personas persona) => datos.Eliminar(persona);
+        public personas InsertarPersona(personas persona) => datos.Insertar(persona);
+    }
+}
