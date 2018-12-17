7fdf95757b3cc9f6f70831652819c7937cdddd7c
AndreitaCanizares <andreita.canizares01@gmail.com>
2018-12-17 09:44:29 -0500

    Practica 10, 11 y 12
    Practica 10, 11 y 12

---
+++ Practica 10/App_Code/IServicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Acceso;
+
+// NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicio" en el código y en el archivo de configuración a la vez.
+[ServiceContract]
+public interface IServicio
+{
+    [OperationContract]
+    List<computadoras> Seleccionar();
+
+    [OperationContract]
+    bool Actualizar(computadoras datos);
+
+    [OperationContract]
+    bool Eliminar(computadoras datos);
+
+    [OperationContract]
+    computadoras Insertar(computadoras datos);
+
+}
---
+++ Practica 10/App_Code/Servicio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Acceso;
+using Logica;
+
+// NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de clase "Servicio" en el código, en svc y en el archivo de configuración a la vez.
+public class Servicio : IServicio
+{
+    Parseo logica = new Parseo();
+    public List<computadoras> Seleccionar()
+    {
+        return logica.SeleccionarTodas();
+    }
+
+    public bool Actualizar(computadoras datos)
+    {
+        return logica.ActualizarComputador(datos);
+    }
+
+    public bool Eliminar(computadoras datos)
+    {
+        return logica.EliminarCompu(datos);
+    }
+
+    public computadoras Insertar(computadoras datos)
+    {
+        return logica.InsertarComputador(datos);
+    }
+}
